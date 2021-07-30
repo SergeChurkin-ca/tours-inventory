f97af951cd3926bf9bf7bae0e411dce069e19c6a
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-07-30 11:28:23 -0400

    refactoring folder

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import Error from "./components/Error";
+import Home from "./components/Home";
+import TourMenu from "./components/TourMenu";
+import Booking from "./components/Booking";
+import ManageInventory from "./components/ManageInventory";
 
-import Error from './pages/Error';
-import Home from './pages/Home';
-import TourMenu from './pages/TourMenu'
-import Booking from './pages/Booking';
-import ManageInventory from './pages/ManageInventory';
-
-import {Route, Switch} from 'react-router-dom';
-import Navbar from './components/Navbar';
-
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 console.log(process.env.REACT_APP_TOUR_API_KEY);
 function App() {
   return (
-   <div>
-    <Navbar />
+    <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exat path="/tourmenu" component={TourMenu}/>
+        <Route exat path="/tourmenu" component={TourMenu} />
         <Route exact path="/booking" component={Booking} />
-        <Route exact path="/manageinventory" component={ManageInventory}/>
+        <Route exact path="/manageinventory" component={ManageInventory} />
         <Route component={Error} />
       </Switch>
-   </div>
-  )
+    </div>
+  );
 }
 
 export default App;
