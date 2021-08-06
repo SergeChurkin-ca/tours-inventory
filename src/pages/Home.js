50a1a69213fb20532df679f96eb795fc394994c1
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-08-06 11:02:22 -0400

    link fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="home">
-      
       <main className="main wrapper">
         <header>
           <h1>tourbees</h1>
           <div className="line"></div>
           <h2>your exclusive tour partner</h2>
-          <a href="/tours-inventory/tourmenu">
+          <Link to="/tourmenu">
             <button className="header-btn">find out</button>
-          </a>
+          </Link>
         </header>
       </main>
-
     </div>
   );
 }
